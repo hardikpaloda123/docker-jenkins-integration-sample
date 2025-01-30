f0dc46d319e43d14824ba006881daea47cb222be
123Deepakshijain <166839227+123Deepakshijain@users.noreply.github.com>
2025-01-30 19:02:56 +0530

    sample test

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -1,6 +1,7 @@
 public class Test {
 
     public static void main(String[] args) {
-        System.out.println("Hello Hardik Jain!!! Nice to see you.");
+        System.out.println("Hello " +
+                "  Hardik Jain!!! Nice to see you.");
     }
 }
